aed6c7b30d403821649283337a17af704edc1e16
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-08-04 20:16:15 -0300

    Update CloudParkingApplication.java

--- src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
+++ src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudParkingApplication {
+public class CloudParkingApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(CloudParkingApplication.class, args);
 	}
-
 }
