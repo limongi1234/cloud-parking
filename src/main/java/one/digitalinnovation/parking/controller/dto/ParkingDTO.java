8eb12db4d89f0a7d92fdcb32107260e9320b84f2
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-08-06 19:37:20 -0300

    Update ParkingDTO.java

--- src/main/java/one/digitalinnovation/parking/controller/dto/ParkingDTO.java
+++ src/main/java/one/digitalinnovation/parking/controller/dto/ParkingDTO.java
@@ -6,48 +6,56 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ParkingDTO {
+public class ParkingDTO 
+{
 
-    private String id;
-    private String license;
-    private String state;
-    private String model;
-    private String color;
+    private String id, license, state, model, color;
+    
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime entryDate;
+    
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime exitDate;
+    
     private Double bill;
 
-    public String getId() {
+    public String getId() 
+    {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(String id) 
+    {
         this.id = id;
     }
 
-    public String getLicense() {
+    public String getLicense()
+    {
         return license;
     }
 
-    public void setLicense(String license) {
+    public void setLicense(String license) 
+    {
         this.license = license;
     }
 
-    public String getState() {
+    public String getState()
+    {
         return state;
     }
 
-    public void setState(String state) {
+    public void setState(String state) 
+    {
         this.state = state;
     }
 
-    public String getModel() {
+    public String getModel()
+    {
         return model;
     }
 
-    public void setModel(String model) {
+    public void setModel(String model)
+    {
         this.model = model;
     }
 
@@ -55,31 +63,38 @@ public class ParkingDTO {
         return color;
     }
 
-    public void setColor(String color) {
+    public void setColor(String color)
+    {
         this.color = color;
     }
 
-    public LocalDateTime getEntryDate() {
+    public LocalDateTime getEntryDate() 
+    {
         return entryDate;
     }
 
-    public void setEntryDate(LocalDateTime entryDate) {
+    public void setEntryDate(LocalDateTime entryDate) 
+    {
         this.entryDate = entryDate;
     }
 
-    public LocalDateTime getExitDate() {
+    public LocalDateTime getExitDate() 
+    {
         return exitDate;
     }
 
-    public void setExitDate(LocalDateTime exitDate) {
+    public void setExitDate(LocalDateTime exitDate) 
+    {
         this.exitDate = exitDate;
     }
 
-    public Double getBill() {
+    public Double getBill() 
+    {
         return bill;
     }
 
-    public void setBill(Double bill) {
+    public void setBill(Double bill) 
+    {
         this.bill = bill;
     }
 }
