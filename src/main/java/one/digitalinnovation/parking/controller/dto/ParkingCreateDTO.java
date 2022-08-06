c10ab8c6c42106638adcdf666d98b4e0010b3414
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-08-06 19:34:16 -0300

    Update ParkingCreateDTO.java

--- src/main/java/one/digitalinnovation/parking/controller/dto/ParkingCreateDTO.java
+++ src/main/java/one/digitalinnovation/parking/controller/dto/ParkingCreateDTO.java
@@ -1,41 +1,50 @@
 package one.digitalinnovation.parking.controller.dto;
 
-public class ParkingCreateDTO {
+public class ParkingCreateDTO 
+{
 
     private String license;
     private String state;
     private String model;
     private String color;
 
-    public String getLicense() {
+    public String getLicense() ]
+    {
         return license;
     }
 
-    public void setLicense(String license) {
+    public void setLicense(String license) 
+    {
         this.license = license;
     }
 
-    public String getState() {
+    public String getState() 
+    {
         return state;
     }
 
-    public void setState(String state) {
+    public void setState(String state) 
+    {
         this.state = state;
     }
 
-    public String getModel() {
+    public String getModel() 
+    {
         return model;
     }
 
-    public void setModel(String model) {
+    public void setModel(String model) 
+    {
         this.model = model;
     }
 
-    public String getColor() {
+    public String getColor() 
+    {
         return color;
     }
 
-    public void setColor(String color) {
+    public void setColor(String color) 
+    {
         this.color = color;
     }
 }
